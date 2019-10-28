63a0408f26af13114b7e2038a36d38f6dd23733c
Ayaka Tanabe <tanabe0kitkat@gmail.com>
2019-10-28 11:52:00 +0900

    extract method

--- video_rental.py
+++ video_rental.py
@@ -45,6 +45,8 @@ class Customer:
 
     def __init__(self, name):
         self.__name = name
+        self.total_amount = 0.0
+        self.frequent_renter_points = 0
 
     def add_rental(self, rental):
         self.__rentals.append(rental)
@@ -52,40 +54,48 @@ class Customer:
     @property
     def name(self):
         return self.__name
+    
+    def add_rental_price(self, rental):
+      this_amount = 0.0
+      # 一行ごとに金額を計算
+      if rental.movie.price_code == Movie.REGULAR:
+          this_amount += 2.0
+          if rental.days_rented > 2:
+              this_amount += (rental.days_rented - 2) * 1.5
+      elif rental.movie.price_code == Movie.NEW_RELEASE:
+          this_amount += rental.days_rented * 3
+      elif rental.movie.price_code == Movie.CHILDRENDS:
+          this_amount += 1.5
+          if rental.days_rented > 3:
+              this_amount += (rental.days_rented - 3) * 1.5
+      return this_amount
+    
+    def add_rental_point(self, rental):
+      self.frequent_renter_points += 1
+      # 新作を二日以上借りた場合はボーナスポイント
+      if rental.movie.price_code == Movie.NEW_RELEASE and \
+              rental.days_rented > 1:
+          self.frequent_renter_points += 1
 
     def statement(self):
-        total_amount = 0.0
-        frequent_renter_points = 0
-        result = "Rental Record for " + self.name + os.linesep
+        rentals = []
         for rental in self.__rentals:
             this_amount = 0.0
-
-            # 一行ごとに金額を計算
-            if rental.movie.price_code == Movie.REGULAR:
-                this_amount += 2.0
-                if rental.days_rented > 2:
-                    this_amount += (rental.days_rented - 2) * 1.5
-            elif rental.movie.price_code == Movie.NEW_RELEASE:
-                this_amount += rental.days_rented * 3
-            elif rental.movie.price_code == Movie.CHILDRENDS:
-                this_amount += 1.5
-                if rental.days_rented > 3:
-                    this_amount += (rental.days_rented - 3) * 1.5
-
-            # レンタルポイントを加算
-            frequent_renter_points += 1
-            # 新作を二日以上借りた場合はボーナスポイント
-            if rental.movie.price_code == Movie.NEW_RELEASE and \
-                    rental.days_rented > 1:
-                frequent_renter_points += 1
-
-            # この貸し出しに関する数値の表示
-            result += '\t' + rental.movie.title + \
-                '\t' + str(this_amount) + os.linesep
-            total_amount += this_amount
-
-        # フッタ部分の追加
-        result += "Amount owed is " + str(total_amount) + os.linesep
-        result += "You earned " + str(frequent_renter_points) + \
+            this_amount = self.add_rental_price(rental)
+            self.add_rental_point(rental)
+            self.total_amount += this_amount
+            rentals.append({'rental': rental, 'this_amount': this_amount})
+        return self.return_result(rentals)
+    
+    def return_result(self, rentals):
+      result = ""
+      result += "Rental Record for " + self.name + os.linesep
+      for rental in rentals:
+        result += '\t' + rental['rental'].movie.title + \
+                '\t' + str(rental['this_amount']) + os.linesep
+      result += "Amount owed is " + str(self.total_amount) + os.linesep
+      result += "You earned " + str(self.frequent_renter_points) + \
             " frequent renter points"
-        return result
+      return result
+
+
